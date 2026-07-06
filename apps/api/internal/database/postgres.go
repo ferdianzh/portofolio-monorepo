a1869dca108d6f65fac58535b05cb5229955faa1
ferdianzh <ferdianzh30@gmail.com>
2026-07-06 08:41:31 +0700

    update env and db connection

--- apps/api/internal/database/postgres.go
+++ apps/api/internal/database/postgres.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -18,7 +19,7 @@ func Connect() {
 	}
 
 	var conErr error
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	DB, conErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if conErr != nil {
