187127be960752e6d349d02362ad14f9c373d53f
ferdianzh <ferdianzh30@gmail.com>
2026-08-27 10:04:39 +0700

    fix user roleId dto

--- apps/api/internal/user/user.dto.go
+++ apps/api/internal/user/user.dto.go
@@ -1,16 +1,21 @@
 package user
 
+import "github.com/google/uuid"
+
 type CreateUserDTO struct {
 	Name     string `validate:"required,min=3,max=50" json:"name"`
 	Email    string `validate:"required,email" json:"email"`
 	Password string `validate:"required,min=8,strong_password" json:"password"`
+	RoleID   string `validate:"required" json:"role_id"`
 }
 
 func (d CreateUserDTO) ToModel() User {
+	rid, _ := uuid.Parse(d.RoleID)
 	return User{
 		Name:     d.Name,
 		Email:    d.Email,
 		Password: d.Password,
+		RoleID:   &rid,
 	}
 }
 
@@ -18,12 +23,19 @@ type UpdateUserDTO struct {
 	Name     string `validate:"omitempty,min=3,max=50" json:"name"`
 	Email    string `validate:"omitempty,email" json:"email"`
 	Password string `validate:"omitempty,min=8,strong_password" json:"password"`
+	RoleID   string `validate:"omitempty" json:"role_id"`
 }
 
 func (d UpdateUserDTO) ToModel() User {
+	var rid *uuid.UUID
+	if d.RoleID != "" {
+		tmp, _ := uuid.Parse(d.RoleID)
+		rid = &tmp
+	}
 	return User{
 		Name:     d.Name,
 		Email:    d.Email,
 		Password: d.Password,
+		RoleID:   rid,
 	}
 }
